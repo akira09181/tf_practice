aef09c1c488e248823bc896f6be7b644931e35bb
akira09181 <freelance0918@gmail.com>
2022-11-07 22:30:10 +0900

    subnet add

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,23 @@
 terraform {
-    required_version = ">=1.3"
-    required_providers{
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>4.0"
-        }
+  required_version = ">=1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.0"
     }
+  }
 }
 
 provider "aws" {
-    profile = "terraform"
-    region = "ap-northeast-1"
+  profile = "terraform"
+  region  = "ap-northeast-1"
 }
 
 variable "project" {
-    type = string
+  type = string
 }
 
 variable "envionment" {
-    type = string
+  type = string
 }
 
--- terraform/network.tf
+++ terraform/network.tf
@@ -11,3 +11,60 @@ resource "aws_vpc" "vpc" {
     Env     = var.envionment
   }
 }
+
+
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.envionment}-public_subnet_1a"
+    project = var.project
+    Env     = var.envionment
+    Type    = "public"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "192.168.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.envionment}-public_subnet_1a"
+    project = var.project
+    Env     = var.envionment
+    Type    = "public"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.3.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.envionment}-public_subnet_1a"
+    project = var.project
+    Env     = var.envionment
+    Type    = "public"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "192.168.4.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.envionment}-public_subnet_1a"
+    project = var.project
+    Env     = var.envionment
+    Type    = "public"
+  }
+}
