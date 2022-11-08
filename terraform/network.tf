2141f6b8077920bb0ad3b1d7682efb6fe7f9a60a
akira09181 <freelance0918@gmail.com>
2022-11-09 05:17:08 +0900

    route table and gateway

--- terraform/network.tf
+++ terraform/network.tf
@@ -68,3 +68,59 @@ resource "aws_subnet" "private_subnet_1c" {
     Type    = "public"
   }
 }
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.envionment}-public_rt"
+    project = var.project
+    Env     = var.envionment
+    Type    = "public"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_1a" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1a.id
+}
+
+resource "aws_route_table_association" "public_rt_1c" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1c.id
+}
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.envionment}-private_rt"
+    project = var.project
+    Env     = var.envionment
+    Type    = "private"
+  }
+}
+resource "aws_route_table_association" "private_rt_1a" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1a.id
+}
+
+resource "aws_route_table_association" "private_rt_1c" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1c.id
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.envionment}-igw"
+    Project = var.project
+    Env     = var.envionment
+  }
+}
+
+resource "aws_route" "public_rt_igw_r" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
